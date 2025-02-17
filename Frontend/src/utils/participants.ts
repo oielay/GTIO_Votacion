9d08b9ae90d39b4b07e208bf8e101e1750e59cfe
Oier Layana <oierlayana@gmail.com>
2025-02-17 20:01:09 +0100

    feat (frontend): Creación de seccion de votaciones. #11

--- Frontend/src/utils/participants.ts
+++ Frontend/src/utils/participants.ts
@@ -2,24 +2,28 @@ const PARTICIPANTS = [
     {
         name: "Pedro Sánchez",
         image: "/participant1.jpg",
+        image_voting: "/participant1-voting.webp",
         description:
         "Pedro Sánchez Pérez-Castejón es un político español, presidente del Gobierno de España desde junio de 2018. Licenciado en Ciencias Económicas y Empresariales por la Universidad Camilo José Cela, es miembro del Partido Socialista Obrero Español (PSOE).",
     },
     {
         name: "Mariano Rajoy",
         image: "/participant2.jpg",
+        image_voting: "/participant2-voting.webp",
         description:
         "Mariano Rajoy Brey es un político español, presidente del Gobierno de España desde diciembre de 2011 hasta junio de 2018. Licenciado en Derecho por la Universidad de Santiago de Compostela, es miembro del Partido Popular (PP).",
     },
     {
         name: "Albert Rivera",
         image: "/participant3.jpg",
+        image_voting: "/participant3-voting.webp",
         description:
         "Albert Rivera Díaz es un político español, presidente de Ciudadanos desde 2006 hasta 2019. Licenciado en Derecho por la Universidad Ramon Llull, es miembro de Ciudadanos.", 
     },
     {
         name: "Pablo Iglesias",
         image: "/participant4.jpg",
+        image_voting: "/participant4-voting.webp",
         description:
         "Pablo Iglesias Turrión es un político español, secretario general de Podemos desde 2014. Licenciado en Derecho por la Universidad Complutense de Madrid, es miembro de Podemos.",
     },
