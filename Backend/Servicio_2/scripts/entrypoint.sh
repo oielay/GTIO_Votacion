cb02a8e8c3421366d0c9e40ccb04b5d22a94e55b
Oier Alduncin <alduncin.139998@e.unavarra.es>
2025-03-12 18:54:39 +0100

    fix (backend): Securización de contraseña de la base de datos en script #40

--- Backend/Servicio_2/scripts/entrypoint.sh
+++ Backend/Servicio_2/scripts/entrypoint.sh
@@ -2,5 +2,5 @@
 echo "Esperando a que SQL Server inicie..."
 sleep 10
 echo "Ejecutando script.sql..."
-/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P Password123! -C -d master -i /var/opt/mssql/init-scripts/init.sql
+/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P "$MSSQL_SA_PASSWORD" -C -d master -i /var/opt/mssql/init-scripts/init.sql
 wait
